59492d65b61169e00b931e85179b65b04a7b3c40
aipark-dev-01 <dev@aipark.ai>
2024-11-07 13:51:09 +0900

    fix: close 버튼 안눌리는 문제

--- src/VideoPlayer.tsx
+++ src/VideoPlayer.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function VideoPlayer({ src, onEnded, onClose }: Props) {
   return (
     <div className="w-full relative">
-      <button type="button" className="w-[100px] h-[100px] absolute right-[100px] top-0" onClick={onClose}>
+      <button type="button" className="w-[100px] h-[100px] absolute right-[100px] top-0 z-[999]" onClick={onClose}>
         <div className="w-[120px] h-[14px] bg-orange-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45"></div>
         <div className="w-[120px] h-[14px] bg-orange-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-45"></div>
       </button>
